eb9b8412e0c84780b4b7c167537af2acd907ce0a
kvs6270 <karansharma6270@gmail.com>
2026-02-28 05:41:09 +0000

    Add a welcome page for root route

---
+++ workspace/src/components/Pages/Welcome.jsx
@@ -0,0 +1,10 @@
+import { Navbar } from "../Cogs/Navbar"
+
+export function Welcome() {
+    return (
+        <div>
+            <Navbar />
+            <div>Welcome</div>
+        </div>
+    )
+}
