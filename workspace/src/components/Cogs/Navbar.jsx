a636b91b0157dc8bb9753efc6dc1f479d1f6ee0d
kvs6270 <karansharma6270@gmail.com>
2026-03-12 12:13:52 +0000

    Style the Navbar

--- workspace/src/components/Cogs/Navbar.jsx
+++ workspace/src/components/Cogs/Navbar.jsx
@@ -1,12 +1,15 @@
 import { Link } from "react-router-dom"
+import style from "../CogCSS/Navbar.module.css"
 export function Navbar() {
     return(
-        <ul>
-            <li><Link to="/Back">Back</Link></li>
-            <li><Link to="/Home">Home</Link></li>
-            <li><Link to="/Recommendation">Recommendation</Link></li>
-            <li><Link to="/Watched">Watched</Link></li>
-            <li><Link to="/ToWatch">ToWatch</Link></li>
-        </ul>
+        <div className={style.navbar}>
+            <ul>
+                <li><Link to="/Back">Back</Link></li>
+                <li><Link to="/Home">Home</Link></li>
+                <li><Link to="/Recommendation">Recommendation</Link></li>
+                <li><Link to="/Watched">Watched</Link></li>
+                <li><Link to="/ToWatch">ToWatch</Link></li>
+            </ul>
+        </div>
     )
 }
