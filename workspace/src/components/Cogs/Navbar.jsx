f65a8cc10923d8621e39df52ddb7e65ab5dbb261
kvs6270 <karansharma6270@gmail.com>
2026-03-27 13:25:19 +0000

    Re-Design the Navbar
    Include the website Icon and Name.

--- workspace/src/components/Cogs/Navbar.jsx
+++ workspace/src/components/Cogs/Navbar.jsx
@@ -1,15 +1,27 @@
 import { Link } from "react-router-dom"
 import style from "../CogCSS/Navbar.module.css"
+import icon from "./icon.png"
 export function Navbar() {
     return(
         <div className={style.navbar}>
-            <ul>
-                <li><Link to="/">Back</Link></li>
-                <li><Link to="/Home">Home</Link></li>
-                <li><Link to="/Recommendation">Recommendation</Link></li>
-                <li><Link to="/Watched">Watched</Link></li>
-                <li><Link to="/ToWatch">ToWatch</Link></li>
-            </ul>
+
+
+            <div className={style.contentContainer}>
+                <div className={style.Logo}>
+                    <img className = {style.LogoImage} src={icon} alt="Qflicks" />
+                    <p>FLICKS</p>
+                
+                </div>
+                <ul>
+                    <li><Link to="/">Back</Link></li>
+                    <li><Link to="/Home">Home</Link></li>
+                    <li><Link to="/Recommendation">Recommendation</Link></li>
+                    <li><Link to="/Watched">Watched</Link></li>
+                    <li><Link to="/ToWatch">ToWatch</Link></li>
+                </ul>
+            </div>
+
+
         </div>
     )
 }
