55e562bf4f8b491cbcf24d6c273b426a9d11c301
kvs6270 <karansharma6270@gmail.com>
2026-06-12 09:07:37 +0000

    Initialize a new GridTile to component
    To define an individual tile component for the MovieGrid page.

---
+++ workspace/src/components/Cogs/GridTile.jsx
@@ -0,0 +1,38 @@
+
+
+import {useNavigate} from 'react-router'
+import style from "../CogCSS/GridTile.module.css"
+
+export function Tile({movieObj, children}) {   
+
+    if (movieObj == null) {
+
+        return (
+            <div className= {style.tile} >
+            {children}
+        </div>
+        )
+    }
+
+    
+
+    const navigate = useNavigate();
+
+
+    const title = movieObj.title;
+
+    const bgImg = movieObj.poster
+
+
+    
+
+
+    return (
+
+        
+        <div style={{backgroundImage: `URL(${bgImg})`}} className= {style.tile} onClick={() => {
+            navigate(`/Movie/${movieObj.id}`)}}>
+            
+        </div>
+    )
+}
